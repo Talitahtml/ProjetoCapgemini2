37e39ab5ff39efcbdc10bcb12a127e0a6b6902ef
Talitahtml <97572310+Talitahtml@users.noreply.github.com>
2022-11-29 18:14:34 -0300

    Add files via upload

---
+++ Javacap2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacap2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elica
+ */
+public class Javacap2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+       Scanner sc = new Scanner (System.in);
+        int x;
+        x = sc. nextInt();
+        System.out.println("voce digitou:"+ x);
+        sc.close();
+    }
+    
+}
